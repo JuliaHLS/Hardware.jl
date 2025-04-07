1211aa5855a37439ec8fdb0fa79e85473c62c220
Benedict Short <benedictnjshort@gmail.com>
2025-04-07 12:06:15 +0100

    Fixing bindings for LLVM v18, MLIR v18 and Julia 1.12.0-beta

--- deps/build.jl
+++ deps/build.jl
@@ -1,3 +1,3 @@
 # generate the Bindings for HLSTool
-make_bindings_path = joinpath(@__DIR__, "../bindings/make_bindings.jl")
+make_bindings_path = joinpath(@__DIR__, "./bindings/make_bindings.jl")
 include(make_bindings_path)
